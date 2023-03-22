e6a33e96043ede20312a5245e4649a2261e7746a
Yanzzzi <nekit.d-k@yandex.ru>
2023-03-22 19:36:31 +0400

    added admin panel

--- treemenu/menu/admin.py
+++ treemenu/menu/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from menu.models import Menu, Item
 
-# Register your models here.
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Item)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'parent')
+    list_filter = ('menu',)
+    prepopulated_fields = {'slug': ('title',)}
+    fieldsets = (
+        ('Add new item', {
+            'description': "Parent should be a menu or item",
+            'fields': (('menu', 'parent'), 'title', 'slug')
+        }),
+    )
