e7e8fea3bc5d1fe56e65dc3f544e2d1ab0f14cf0
Yanzzzi <nekit.d-k@yandex.ru>
2023-03-22 18:57:11 +0400

    create templates, added view and urls

--- treemenu/menu/views.py
+++ treemenu/menu/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class MenuView(TemplateView):
+    template_name = 'menu/menu.html'
