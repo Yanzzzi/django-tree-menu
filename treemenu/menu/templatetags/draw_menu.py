2d19ff299223dc18f20fe9900d20750ed659160a
Yanzzzi <nekit.d-k@yandex.ru>
2023-03-25 02:37:25 +0400

    added tree menu rendering, changed the algorithm for constructing the menu context

--- treemenu/menu/templatetags/draw_menu.py
+++ treemenu/menu/templatetags/draw_menu.py
@@ -1,5 +1,6 @@
 from django import template
-from menu.models import Item, Menu
+from menu.models import Item
+from django.db.models import Q
 
 register = template.Library()
 
@@ -9,36 +10,39 @@ def get_type(value):
     return type(value).__name__
 
 
-@register.inclusion_tag('menu/draw.html', takes_context=True)
-def draw_menu(context, main_menu):
-    items = list(Menu.objects.values('items__pk', 'items__title', 'items__childrens', 'items__parent', 'items__slug').filter(title=main_menu))
-    childs = {}
-    for i in items:
-        print(i)
-    print('-'*100)
-    items_to_render = {'menu_name': main_menu, main_menu: {}}
-    for item in items:
-        if not item['items__childrens'] and not item['items__parent']:
-            items_to_render[main_menu][item['items__title']] = item['items__slug']
-        elif item['items__childrens'] and not item['items__parent']:
-            if item['items__title'] not in items_to_render[main_menu]:
-                items_to_render[main_menu][item['items__title']] = {'child_ids': []}
-            items_to_render[main_menu][item['items__title']]['child_ids'].append(item['items__childrens'])
-        elif not item['items__childrens'] and item['items__parent']:
-            childs[item['items__pk']] = items[item['items__pk']]
-        else:
-            # TODO сделать кейс когда есть и родитель и ребенок
-            pass
-    print('childs')
-    print(childs)
-    while childs:
-        for k, v in items_to_render[main_menu].items():
-            if type(v) == dict:
-                child_id = v['child_ids'].pop(0)
-                child = childs.pop(child_id)
-                v[child['items__title']] = child['items__slug']
-                if not childs:
-                    del v['child_ids']
-    print('###########context############')
-    print(items_to_render)
+@register.inclusion_tag('menu/draw.html')
+def draw_menu(main_menu):
+    items = Item.objects.filter(menu__title=main_menu)
+    items_val = list(items.values())
+
+    items_with_childs = items.filter(childrens__isnull=False).values('childrens')
+
+    items_to_render = get_items_dict(items, items_val, items_with_childs)
+    menu_items = {'menu_name': main_menu, 'items': items_to_render}
+    return menu_items
+
+
+def get_items_dict(items, items_val, items_with_childs):
+    items_to_render = {}
+    for item in items_val:
+        if items.filter(Q(pk=item['id']) & Q(childrens__isnull=True)).exists():
+            if check_field_added(items_to_render, item['title']):
+                items_to_render[item['title']] = item['slug']
+        else:  # have childs
+            childs_val = items.filter(Q(pk__in=items_with_childs) & Q(parent_id=item['id'])).values()
+            sub_items = get_items_dict(items, childs_val, items_with_childs)
+            items_to_render[item['title']] = sub_items
+
     return items_to_render
+
+
+def check_field_added(items, item_title):
+    for k, v in items.items():
+        if type(v) == dict:
+            ans = check_field_added(v, item_title)
+            if not ans:
+                return False
+        else:
+            if k == item_title:
+                return False
+    return True
